00f80e843a2faab70b9caa96f91c50d62442ea3d
Hoang Nguyen <contact@hoangnd.me>
2017-07-04 20:43:22 +1000

    mock api data & test cases

--- src/api.js
+++ src/api.js
@@ -2,21 +2,44 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/airlines', (req, res) => {
-  res.json({
-      'airline': 'test',
-  })
+  res.json([{
+    "code": "SQ",
+    "name": "Singapore Airlines"
+  }])
 })
 
 router.get('/airports', (req, res) => {
-  res.json({
-      'airports': 'test',
-  })
+  let query = req.query.q
+  if (query === undefined || query.length < 2)
+    return res.status(400).send('query must contain at least two letters');
+
+  res.json([
+    {
+      "airportCode": "MEL",
+      "airportName": "Tullamarine Arpt",
+      "cityName": "Melbourne",
+      "countryName": "Australia"
+    }
+  ])
 })
 
-router.get('/search', (req, res) => {
-  res.json({
-      'search': 'test',
-  })
+router.get('/search/:code', (req, res) => {
+  let code = req.params.code
+  let {date, from, to} = req.query
+
+  if (date === undefined || from === undefined || to === undefined)
+    return res.status(400).send('date, from & to are required');
+
+  res.json([
+    {
+      "depart": "2018-09-02T13:33:00+10:00",
+      "arrive": "2018-09-02T19:34:00-04:00",
+      "flight": "QF798",
+      "distance": 16014,
+      "durationMin": 1201,
+      "price": 1859.01
+    }
+  ])
 })
 
 module.exports = router
--- src/tests/api.spec.js
+++ src/tests/api.spec.js
@@ -9,34 +9,100 @@ describe('API', () => {
 
   describe('GET /airlines', () => {
     it('it should return success', (done) => {
-        chai.request(server)
-          .get('/api/airlines')
-          .end((err, res) => {
-            res.should.have.status(200)
-            done()
+      chai.request(server)
+        .get('/api/airlines')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.body.map((item) => {
+            item.should.have.all.keys(['code', 'name'])
           })
+          done()
+        })
     })
   })
 
   describe('GET /airports', () => {
     it('it should return success', (done) => {
-        chai.request(server)
-          .get('/api/airports')
-          .end((err, res) => {
-            res.should.have.status(200)
-            done()
+      chai.request(server)
+        .get('/api/airports?q=Melbourne')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.body.map((item) => {
+            item.should.have.all.keys(['airportCode', 'airportName', 'cityName', 'countryName'])
           })
+          done()
+        })
+    })
+
+    it('it should fail if query contains less than 2 letters', (done) => {
+      chai.request(server)
+        .get('/api/airports?q=M')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('it should fail if query is missing', (done) => {
+      chai.request(server)
+        .get('/api/airports')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
     })
   })
 
   describe('GET /search', () => {
     it('it should return success', (done) => {
-        chai.request(server)
-          .get('/api/search')
-          .end((err, res) => {
-            res.should.have.status(200)
-            done()
+      chai.request(server)
+        .get('/api/search/QF?date=2018-09-02&from=SYD&to=JFK')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.body.map((item) => {
+            item.should.have.all.keys(['depart', 'arrive', 'flight', 'distance', 'durationMin', 'price'])
           })
+          done()
+        })
+    })
+
+    it('it should fail if query is missing', (done) => {
+      chai.request(server)
+        .get('/api/search/QF')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('it should fail if date is missing', (done) => {
+      chai.request(server)
+        .get('/api/search/QF?from=SYD&to=JFK')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('it should fail if from is missing', (done) => {
+      chai.request(server)
+        .get('/api/search/QF?date=2018-09-02&to=JFK')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
+    })
+
+    it('it should fail if to is missing', (done) => {
+      chai.request(server)
+        .get('/api/search/QF?date=2018-09-02from=SYD')
+        .end((err, res) => {
+          res.should.have.status(400)
+          done()
+        })
     })
   })
 
